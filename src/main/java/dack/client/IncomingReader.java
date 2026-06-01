d853e749a6782fa522ee9582247cf62ef865125a
dngnguyen <osmiumshark@gmail.com>
2026-06-01 17:23:35 +0700

    U03.5: Update layout

--- src/main/java/dack/client/IncomingReader.java
+++ src/main/java/dack/client/IncomingReader.java
@@ -43,13 +43,30 @@ public class IncomingReader extends Thread{
                     String time = parts[3];
 
                     SwingUtilities.invokeLater(() -> {
-                        String htmlMsg = "<table width='100%' cellpadding='0' cellspacing='0' style='margin-bottom:5px;'>"
-                            + "<tr>"
-                            + "<td>" + sender + ": " + content + "</td>"
-                            + "<td align='right' nowrap='nowrap'>[" + time + "]</td>"
-                            + "</tr>"
-                            + "</table>";
+                        String htmlMsg = buildChatRow(sender, content, time, false);
                         appendHtmlMessage(htmlMsg);
+                    });
+
+                } else if (action.equals("HISTORY") && parts.length >= 4) {
+
+                    String sender = parts[1];
+                    String content = parts[2];
+                    String time = parts[3];
+
+                    SwingUtilities.invokeLater(() -> {
+                        // Tin lịch sử hiển thị mờ hơn để phân biệt với tin mới
+                        String htmlMsg = buildChatRow(sender, content, time, true);
+                        appendHtmlMessage(htmlMsg);
+                    });
+
+                } else if (action.equals("HISTORY_END")) {
+
+                    SwingUtilities.invokeLater(() -> {
+                        String separator = "<div style='border-top: 1px dashed #aaaaaa; "
+                                + "color: #888888; font-size: 10px; text-align: center; "
+                                + "margin: 6px 0; padding: 2px;'>"
+                                + "--- Tin nhắn mới ---</div>";
+                        appendHtmlMessage(separator);
                     });
 
                 } else if (action.equals("SYSTEM") && parts.length >= 2) {
@@ -88,5 +105,33 @@ public class IncomingReader extends Thread{
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Escape các ký tự HTML đặc biệt để tránh vỡ layout khi nội dung
+     * chứa <, >, &, ", '
+     */
+    private String escapeHtml(String text) {
+        if (text == null) return "";
+        return text.replace("&", "&amp;")
+                   .replace("<", "&lt;")
+                   .replace(">", "&gt;")
+                   .replace("\"", "&quot;");
+    }
+
+    /**
+     * Tạo HTML row cho một tin nhắn.
+     * @param isHistory true = tin lịch sử (màu mờ), false = tin mới (màu bình thường)
+     */
+    private String buildChatRow(String sender, String content, String time, boolean isHistory) {
+        String color = isHistory ? "color: #888888;" : "";
+        return "<table width='100%' cellpadding='0' cellspacing='0' "
+                + "style='margin-bottom:5px; " + color + "'>"
+                + "<tr>"
+                + "<td>" + escapeHtml(sender) + ": " + escapeHtml(content) + "</td>"
+                + "<td align='right' nowrap='nowrap' style='font-size:10px; padding-left:8px;'>"
+                + "[" + escapeHtml(time) + "]</td>"
+                + "</tr>"
+                + "</table>";
     }
 }
